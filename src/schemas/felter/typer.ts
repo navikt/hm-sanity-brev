b91b88fcc659fe389c504dd1c6e4a3dd890178de
matsbyfl <mats@byfuglien.net>
2023-03-23 15:36:38 +0100

    Oppdaterer flettefelter

--- src/schemas/felter/typer.ts
+++ src/schemas/felter/typer.ts
@@ -1,5 +1,4 @@
 export const flettefelter = [
-  { title: 'Barnets navn', value: 'barnetsNavn' },
   { title: 'Barnets fødselsnummer', value: 'barnetsFodselsnummer' },
   { title: 'Beløp', value: 'belop' },
   { title: 'Sats', value: 'sats' },
@@ -9,6 +8,14 @@ export const flettefelter = [
   { title: 'Neste kravdato', value: 'nesteKravdato' },
   { title: 'Utbetales til navn', value: 'utbetalesTilNavn' },
   { title: 'Utbetales til kontonr', value: 'utbetalesTilKontonr' },
+  { title: 'Bestillingsdato', value: 'bestillingsDato' },
+  { title: 'Barnets fulle navn', value: 'barnetsFulleNavn' },
+  { title: 'Mottatt dato', value: 'motattDato' },
+  { title: 'Barnets fornavn', value: 'barnetsFornavn' },
+  { title: 'Sfærisk styrke høyre øye', value: 'sfæriskStyrkeHøyre' },
+  { title: 'Cylinderstyrke høyre øye', value: 'cylinderstyrkeHøyre' },
+  { title: 'Sfærisk styrke venstre øye', value: 'sfæriskStyrkeVenstre' },
+  { title: 'Cylinderstyrke venstre øye', value: 'cylinderstyrkeVenstre' },
 ]
 
 export const hjemler = ['2', '3', '4', '6', '13']
@@ -16,6 +23,8 @@ export const hjemler = ['2', '3', '4', '6', '13']
 export const avslagFlettefelter = [
   { title: 'Bestillingsdato', value: 'bestillingsDato' },
   { title: 'Forrige dato for briller', value: 'forrigeBrilleDato' },
-  { title: 'Sfærisk styrke', value: 'sfæriskStyrke' },
-  { title: 'Cylinderstyrke', value: 'cylinderstyrke' }
+  { title: 'Sfærisk styrke høyre øye', value: 'sfæriskStyrkeHøyre' },
+  { title: 'Cylinderstyrke høyre øye', value: 'cylinderstyrkeHøyre' },
+  { title: 'Sfærisk styrke venstre øye', value: 'sfæriskStyrkeVenstre' },
+  { title: 'Cylinderstyrke venstre øye', value: 'cylinderstyrkeVenstre' },
 ]
