7975faa9f308f89db6ca9b91ea5dc44d6dac92c0
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2023-10-23 13:43:13 +0200

    Ny betingelse: Vise NAV-adresse HoT

--- src/schemas/annotasjoner/betingelser.ts
+++ src/schemas/annotasjoner/betingelser.ts
@@ -2,5 +2,6 @@ import type { TitledListValue } from 'sanity'
 
 export const betingelser: ReadonlyArray<TitledListValue<string>> = [
   { title: 'Beløp mindre enn sats', value: 'belopMindreEnnSats' },
-  { title: 'Vise NAV-adresse', value: 'viseNavAdresse' },
+  { title: 'Vise NAV-adresse Agder', value: 'viseNavAdresse' },
+  { title: 'Vise NAV-adresse HoT', value: 'viseNavAdresseHoT' },
 ]
